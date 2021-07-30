80a1c37898b8187ded105fa5d8aee93b839a813e
micro-hawk <dasvikas0071@gmail.com>
2021-07-30 10:23:06 +0530

    function changed a bit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 document.addEventListener("mousemove", parallax);
-function parallax (e) {
-    this.querySelectorAll('.animate').forEach(layer => {
+
+ function parallax(e) {
+    this.querySelectorAll('.animate').forEach(layer => 
+        {
         const speed = layer.getAttribute('data-speed');
-        const x = (window.innerWidth - e.pageX*speed)/100;
-        const y = (window.innerHeight - e.pageY*speed)/100;
+        const x = (window.innerWidth - e.pageX*speed)/200;
+        const y = (window.innerHeight - e.pageY*speed)/200;
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
     })
-}
+};
+
+
